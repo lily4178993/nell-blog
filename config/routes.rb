04c3e1d37c694b2f2c5469bd42f5404a5754b02b
lily4178993 <nellytelli@gmail.com>
2023-12-13 14:03:48 +0000

    feat: Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "users#index" # Defines the root path route ("/")
-  get 'users' => 'users#index', as: :users
-  get 'users/:id' => 'users#show', as: :user
-  get 'users/:user_id/posts' => 'posts#index', as: :user_posts
-  get 'users/:user_id/posts/:id' => 'posts#show', as: :user_post
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
