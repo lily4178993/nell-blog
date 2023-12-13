cf68098d68caf593db4188df5fdd10d9e571334e
lily4178993 <nellytelli@gmail.com>
2023-12-13 12:39:53 +0000

    feat: Update routes.rb to define routes for users, posts, likes, and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index" # Defines the root path route ("/")
+  get 'users' => 'users#index', as: :users
+  get 'users/:id' => 'users#show', as: :user
+  get 'users/:user_id/posts' => 'posts#index', as: :user_posts
+  get 'users/:user_id/posts/:id' => 'posts#show', as: :user_post
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :likes, only: [:create, :destroy]
+      resources :comments, only: [:new, :create, :destroy]
+    end
+  end
 end
