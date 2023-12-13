a992979a09954ee4fb8afeac7028f24efd7ead0b
lily4178993 <nellytelli@gmail.com>
2023-12-13 12:34:33 +0000

    feat: Add PostsController and views for index, show, new, create, and destroy actions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @posts = @user.recent_posts
+  end
+
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @post = @user.posts.find_by(id: params[:id])
+    @comments = @user.posts.all
+  end
+
+  def new
+    @user = User.find_by(id: params[:user_id])
+    @post = @user.posts.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash.now[:error] = 'Post could not be created'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to user_posts_path
+  end
+end
