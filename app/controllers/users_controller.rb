c246e96160302b256ce797e52aaeea3a3380b367
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-19 15:28:13 +0100

    fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.includes(posts: [:comments, :likes]).find_by(id: params[:id])
+    @user = User.includes(posts: %i[comments likes]).find_by(id: params[:id])
     @posts = @user.recent_posts
     @post = Post.new
     @like = @posts.present? ? Like.find_by(user: current_user, post: @posts.first) : nil
