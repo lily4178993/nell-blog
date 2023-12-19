abae2cde3946f2a361e1a31d9cd0a6a6b34b0d88
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-19 17:01:26 +0100

    Add test cases for user show spec file

---
+++ spec/views/user_show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  let(:user) { User.create(id: 1, name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Lorem ipsum', posts_counter: 5) }
+  let!(:posts) { [Post.create(id: 1, author: user, title: 'Title 1', text: 'Body 1'),
+                  Post.create(id: 2, author: user, title: 'Title 2', text: 'Body 2'),
+                  Post.create(id: 3, author: user, title: 'Title 3', text: 'Body 3')] }
+
+  before do
+    assign(:user, user)
+    assign(:posts, posts)
+    assign(:post, Post.new) # Assign a new instance of Post to @post
+    render
+  end
+
+  it 'displays user details' do
+    expect(rendered).to have_content(user.name)
+    expect(rendered).to have_content(user.bio)
+    expect(rendered).to have_css("img[src*='#{user.photo}']")
+    expect(rendered).to have_content("Number of posts: #{user.posts_counter}")
+  end
+
+  it 'displays user posts' do
+    posts.each do |post|
+      expect(rendered).to have_content(post.title)
+      expect(rendered).to have_content(post.text) # Assuming 'text' is the attribute for post body
+    end
+  end
+end
