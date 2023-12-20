40b7c9ff3ab87d1d893bdcec353ba16ee773fcea
lily4178993 <nellytelli@gmail.com>
2023-12-20 11:46:24 +0000

    feat: Refactor post_index_spec.rb to include multiple posts and comments

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -3,42 +3,37 @@ require 'rails_helper'
 RSpec.feature 'User posts index page', type: :feature do
   include ActionView::Helpers::TextHelper
 
-  let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Lorem ipsum', posts_counter: 5) }
+  let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Lorem ipsum', posts_counter: 3) }
+
+  before do
+    3.times do |i|
+      post = Post.create(author: user, title: "Test Post #{i}", text: 'This is a test post.')
+      3.times { Comment.create(user:, post:, text: 'This is a test comment.') }
+    end
+    visit user_posts_path(user)
+  end
 
   scenario 'User sees user details on index page' do
-    visit user_posts_path(user)
-
     expect(page).to have_css("img[src*='#{user.photo}']")
     expect(page).to have_content(user.name)
     expect(page).to have_content("Number of posts: #{user.posts_counter}")
   end
 
   scenario 'User sees post details on index page' do
-    post = Post.create(author: user, title: 'Test Post', text: 'This is a test post.')
-
-    visit user_posts_path(user)
-
-    expect(page).to have_content(post.title)
-    expect(page).to have_content(truncate(post.text, length: 100))
-
-    expect(page).to have_content("Comments: #{post.comments_counter}")
-    expect(page).to have_content("Likes: #{post.likes_counter}")
+    expect(page).to have_content(Post.first.title)
+    expect(page).to have_content(Post.first.text)
+    expect(page).to have_content(Post.first.comments_counter)
+    expect(page).to have_content(Post.first.likes_counter)
+    expect(page).to have_content(Comment.first.text)
   end
 
-  scenario 'User sees pagination section on index page' do
-    3.times { Post.create(author: user, title: 'Test Post', text: 'This is a test post.') }
-
-    visit user_posts_path(user)
-
+  scenario 'User sees pagination section on index page to see more posts' do
     expect(page).to have_css('.post-container', count: 2)
   end
 
-  scenario 'User redirects to post show page when clicking on a post' do
-    post = Post.create(author: user, title: 'Test Post', text: 'This is a test post.')
-
-    visit user_posts_path(user)
-
-    click_link post.title
-    expect(current_path).to eq(user_post_path(user, post))
+  scenario "User can redirect to a post's show page, when click" do
+    post_to_click = Post.order(created_at: :desc).first
+    find('a', text: post_to_click.title, match: :first).click
+    expect(current_path).to eq(user_post_path(user, post_to_click))
   end
 end
