6295532b9f9ff8e70569bfbf5a3fa8d2fa979d1d
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-21 16:20:23 +0100

    Fix linters error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-    user_params.permit(:name, :email, :bio, :password, :password_confirmation)
+      user_params.permit(:name, :email, :bio, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-    user_params.permit(:email, :password)
+      user_params.permit(:email, :password)
     end
   end
 end
