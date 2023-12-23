45b1517be6065ef28d738623c949819763809c91
lily4178993 <nellytelli@gmail.com>
2023-12-23 00:36:19 +0000

    feat: Add API endpoint for retrieving comments by post

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:index]
+      resources :comments, only: [:index, :create]
     end
   end
 
