3ed438f5cfaebc9c95c32762ccdd567a22759cce
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-23 02:02:12 +0100

    Include create action in API comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,26 +6,26 @@ class Api::V1::CommentsController < ApplicationController
     render json: { success: true, comments_by_post: comments_by_post }
   end
 
-  # def create
-  #   post = current_user.posts.find_by(id: params[:post_id])
+  def create
+    post = current_user.posts.find_by(id: params[:post_id])
 
-  #   if post
-  #     comment = post.comments.build(comment_params)
-  #     comment.user = current_user
+    if post
+      comment = post.comments.build(comment_params)
+      comment.user = current_user
 
-  #     if comment.save
-  #       render json: { success: true, message: 'Comment created successfully' }, status: :created
-  #     else
-  #       render json: { success: false, message: 'Comment could not be created' }, status: :bad_request
-  #     end
-  #   else
-  #     render json: { success: false, message: 'Post not found' }, status: :not_found
-  #   end
-  # end
+      if comment.save
+        render json: { success: true, message: 'Comment created successfully' }, status: :created
+      else
+        render json: { success: false, message: 'Comment could not be created' }, status: :bad_request
+      end
+    else
+      render json: { success: false, message: 'Post not found' }, status: :not_found
+    end
+  end
 
-  # private
+  private
 
-  # def comment_params
-  #   params.require(:comment).permit(:text)
-  # end
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
